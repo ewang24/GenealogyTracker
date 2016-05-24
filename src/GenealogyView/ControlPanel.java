68cd184cf9d0fb1710f546601cbf2a531a9bc570
evan <mistix@comcast.net>
2016-05-24 01:39:48 -0600

    Panel for buttons on west side of screen. Also experimenting

--- src/GenealogyView/ControlPanel.java
+++ src/GenealogyView/ControlPanel.java
@@ -6,15 +6,25 @@ package GenealogyView;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 public class ControlPanel extends JPanel
 {
+	private final int WIDTH = 50;
 	ControlPanel(JFrame frame)
 	{
-		this.setSize(new Dimension(70,frame.getHeight()));
+		FlowLayout p = new FlowLayout();
+		p.setAlignment(FlowLayout.CENTER);
+		this.setLayout(p);
+		this.setPreferredSize(new Dimension(WIDTH,frame.getHeight()));
 		this.setBackground(new Color(216, 228, 248));
+		for(int i = 0; i < 10; i ++)
+			this.add(new JButton("hi"));
+		
+		int i = true ? 1 : 2;
 	}
 }
